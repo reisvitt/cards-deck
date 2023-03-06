d2430a6e049d5250cc4f7d1a1f604c7511f7b82f
Vitor Reis <vitor.reis@izap.com.br>
2023-03-06 04:48:35 -0300

    feat: add the goback button in header

--- src/presentation/components/layout/auth/styles.ts
+++ src/presentation/components/layout/auth/styles.ts
@@ -5,7 +5,7 @@ export const Header = styled.header`
   width: 100%;
   background-color: rgba(255,255,255, 1);
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   padding: 1.5em 2em;
   backdrop-filter: blur(10px);
   -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.15);
@@ -14,5 +14,14 @@ export const Header = styled.header`
 export const Name = styled.h4`
   color: ${theme.colors.primary};
   letter-spacing: 1px;
-  font-size: 1.1em;
+  font-size: ${theme.font.sizes.xsmall};
+`
+
+export const Button = styled.button`
+  color: ${theme.colors.primary};
+  cursor: pointer;
+  font-size: ${theme.font.sizes.xsmall};
+  font-weight: bold;
+  letter-spacing: 1px;
+  background: transparent;
 `
