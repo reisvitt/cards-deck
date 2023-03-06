4073f5d20642890d212092e455d5920f2d8a44d5
Vitor Reis <vitor.reis@izap.com.br>
2023-03-05 23:09:53 -0300

    feat: rick-morty enum

---
+++ src/domain/enums/rick-morty.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-shadow */
+
+export enum STATUS {
+  ALIVE = 'ALIVE',
+  DEAD = 'DEAD',
+  UNKNOWN = 'unknown',
+}
+
+export enum GENDER {
+  FEMALE = 'Female',
+  MALE = 'Male',
+  GENDERLESS = 'Genderless',
+  UNKNOWN = 'unknown',
+}
