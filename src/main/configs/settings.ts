4f734c38fb1682be12e3cf482cc4f7f6cae180e7
Vitor Reis <vitor.reis@izap.com.br>
2023-03-05 23:08:00 -0300

    chore: set api url

--- src/main/configs/settings.ts
+++ src/main/configs/settings.ts
@@ -1,4 +1,4 @@
 export const settings = {
-  API_URL: process.env.REACT_APP_API_URL,
-  TITLE: process.env.REACT_APP_TITLE,
+  API_URL: process.env.REACT_APP_API_URL || 'https://rickandmortyapi.com/api',
+  TITLE: process.env.REACT_APP_TITLE || 'Deck Random',
 }
